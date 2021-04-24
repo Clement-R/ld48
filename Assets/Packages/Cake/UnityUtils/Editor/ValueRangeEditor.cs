e18137ad6d74a01b630e300c370edd937372cac6
C.Rondeau <rondeau.clement@gmail.com>
2021-04-24 17:55:00 +0200

    * fix ValueRange custom drawer

--- Assets/Packages/Cake/UnityUtils/Editor/ValueRangeEditor.cs
+++ Assets/Packages/Cake/UnityUtils/Editor/ValueRangeEditor.cs
@@ -13,17 +13,25 @@ namespace Cake.Data
             EditorGUI.BeginProperty(position, label, property);
 
             // Draw label
-            EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+            var labelRect = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
 
             // Don't make child fields be indented
             var indent = EditorGUI.indentLevel;
             EditorGUI.indentLevel = 1;
 
             // Properties
-            EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.PropertyField(property.FindPropertyRelative("Min"), new GUIContent("Min"));
-            EditorGUILayout.PropertyField(property.FindPropertyRelative("Max"), new GUIContent("Max"));
-            EditorGUILayout.EndHorizontal();
+            var labelSize = 40f;
+            var minLabelRect = new Rect(position.x + 150, position.y, labelSize, position.height);
+            var minRect = new Rect(minLabelRect.x + labelSize + 5f, position.y, 75f, position.height);
+            var maxLabelRect = new Rect(minRect.x + 80f, position.y, labelSize, position.height);
+            var maxRect = new Rect(maxLabelRect.x + labelSize + 5f, position.y, 75f, position.height);
+
+            var propMin = property.FindPropertyRelative("Min");
+            var propMax = property.FindPropertyRelative("Max");
+            EditorGUI.LabelField(minLabelRect, "Min");
+            propMin.floatValue = EditorGUI.FloatField(minRect, propMin.floatValue);
+            EditorGUI.LabelField(maxLabelRect, "Max");
+            propMax.floatValue = EditorGUI.FloatField(maxRect, propMax.floatValue);
 
             // Set indent back to what it was
             EditorGUI.indentLevel = indent;
