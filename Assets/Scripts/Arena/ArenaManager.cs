6f9844667b8423f8524d7b9738b5c95e8493dd12
C.Rondeau <rondeau.clement@gmail.com>
2021-04-24 17:55:12 +0200

    + implement ArenaManager basics

--- Assets/Scripts/Arena/ArenaManager.cs
+++ Assets/Scripts/Arena/ArenaManager.cs
@@ -1,29 +1,67 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 using UnityEngine;
 
+using Cake.Data;
+
 namespace Game
 {
     public class ArenaManager : MonoBehaviour
     {
+        public Action OnArenaFinished;
+
         [SerializeField] private Spawner m_spawner;
-        [SerializeField] private Wave m_wave;
+        [SerializeField] private List<Wave> m_waves;
+        [SerializeField] private ValueRange m_delayBetweenWaves;
 
         private List<Wave> m_currentWaves = new List<Wave>();
+        private int m_lastWaveIndex = -1;
+        private float m_nextSpawn;
+        private bool m_isStarted = false;
+        private bool m_isDone = false;
 
-        [ContextMenu("Test arena")]
-        private void StartArena()
+        [ContextMenu("Start arena")]
+        public void StartArena()
         {
-            var wave = Instantiate(m_wave);
-            wave.Spawn(m_spawner);
-            m_currentWaves.Add(wave);
+            m_isStarted = true;
+            m_lastWaveIndex = -1;
+            NextWave();
         }
 
-        [ContextMenu("Wave done ?")]
-        private void WaveDone()
+        private void NextWave()
         {
-            Debug.Log(m_currentWaves.All(e => e.IsDone()));
+            var nextIndex = m_lastWaveIndex + 1;
+            if (nextIndex >= m_waves.Count)
+            {
+                OnArenaFinished?.Invoke();
+                m_isDone = true;
+                return;
+            }
+
+            var wave = Instantiate(m_waves[nextIndex]);
+            m_currentWaves.Add(wave);
+            wave.Spawn(m_spawner);
+
+            m_lastWaveIndex = nextIndex;
+
+            m_nextSpawn = Time.time + m_delayBetweenWaves.Random();
+        }
+
+        private void Update()
+        {
+            if (!m_isStarted || m_isDone)
+                return;
+
+            // Check next wave
+            if (m_currentWaves.All(e => e.IsDone()) || (m_lastWaveIndex < m_waves.Count && Time.time > m_nextSpawn))
+            {
+                NextWave();
+            }
+
+            // Clear
+            m_currentWaves.RemoveAll(e => e.IsDone());
         }
     }
 }
